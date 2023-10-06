4852db8a21a88dff1f5c708fc6376ad18fcf6a69
Fletcher <fletcherbradley123@outlook.com>
2023-10-06 14:15:44 +1000

    Added more types of file loading

--- python/dataload.py
+++ python/dataload.py
@@ -2,7 +2,8 @@ import pandas as pd
 
 df = pd.read_csv("victoriaaccidents.csv")
 
-objectid = df['OBJECTID']
-date = df['ACCIDENT_DATE']
-temp = df['Temperature']
-
+accidentid = df['OBJECTID']
+accidentdate = df['ACCIDENT_DATE']
+accidenttime = df['ACCIDENT_TIME']
+accidentday = df['DAY_OF_WEEK']
+accidentalcohol = df['ALCOHOLTIME']
